f8e0f5f2f2f585d7390271c2326306724c1a5e0c
Patrick <prm5983@rit.edu>
2020-12-10 17:59:14 -0500

    Added HeadTilt from other unity project to this one

---
+++ VR-TestingGrounds/Assets/Scripts/HeadTilt/FollowHead.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHead : MonoBehaviour
+{
+    [SerializeField] private GameObject head = null;
+
+    private void Update()
+    {
+        Vector3 headUpXZ = Vector3.ProjectOnPlane(head.transform.up, Vector3.up);
+        Vector3 axis = Vector3.Cross(Vector3.up, headUpXZ);
+        float headAngleFromUp = Vector3.SignedAngle(Vector3.up, head.transform.up, axis);
+        transform.rotation = Quaternion.AngleAxis(headAngleFromUp, axis);
+    }
+}
